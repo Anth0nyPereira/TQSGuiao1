602ca979cc1b8244a5c79580c74a47f681729abf
Anth0nyPereira <anthonypereira@ua.pt>
2021-04-08 10:50:54 +0100

    Exercise 3a done

---
+++ aula03/tqs_lab03_chromejupiterextension/src/test/java/ChromeJupiterTest.java
@@ -0,0 +1,27 @@
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@ExtendWith(SeleniumJupiter.class)
+class ChromeJupiterTest {
+
+    @Test
+    void testWithOneChrome(ChromeDriver driver) {
+        driver.get("https://bonigarcia.github.io/selenium-jupiter/");
+        assertThat(driver.getTitle(),
+                containsString("JUnit 5 extension for Selenium"));
+    }
+
+    @Test
+    void testWithTwoChromes(ChromeDriver driver1, ChromeDriver driver2) {
+        driver1.get("http://www.seleniumhq.org/");
+        driver2.get("http://junit.org/junit5/");
+        assertThat(driver1.getTitle(), startsWith("Selenium"));
+        assertThat(driver2.getTitle(), equalTo("JUnit 5"));
+    }
+
+}
